e4cb5b974a707c8fc72a77955131467434c0d689
Tristan PETIT <tristan.petit@etu.univ-grenoble-alpes.fr>
2024-03-29 20:50:21 +0100

    new file:   executableASM 	deleted:    nasm-doc/html/local.css 	deleted:    nasm-doc/html/nasmdo10.html 	deleted:    nasm-doc/html/nasmdo11.html 	deleted:    nasm-doc/html/nasmdo12.html 	deleted:    nasm-doc/html/nasmdo13.html 	deleted:    nasm-doc/html/nasmdoc.css 	deleted:    nasm-doc/html/nasmdoc0.html 	deleted:    nasm-doc/html/nasmdoc1.html 	deleted:    nasm-doc/html/nasmdoc2.html 	deleted:    nasm-doc/html/nasmdoc3.html 	deleted:    nasm-doc/html/nasmdoc4.html 	deleted:    nasm-doc/html/nasmdoc5.html 	deleted:    nasm-doc/html/nasmdoc6.html 	deleted:    nasm-doc/html/nasmdoc7.html 	deleted:    nasm-doc/html/nasmdoc8.html 	deleted:    nasm-doc/html/nasmdoc9.html 	deleted:    nasm-doc/html/nasmdoca.html 	deleted:    nasm-doc/html/nasmdocb.html 	deleted:    nasm-doc/html/nasmdocc.html 	deleted:    nasm-doc/html/nasmdocd.html 	deleted:    nasm-doc/html/nasmdoce.html 	deleted:    nasm-doc/html/nasmdocf.html 	deleted:    nasm-doc/html/nasmdoci.html 	deleted:    nasm-doc/html/nasmlogw.png 	modified:   test.asm 	modified:   test.o

--- test.asm
+++ test.asm
@@ -1,23 +1,5 @@
 section .data
-    chocolat db 12
-    newline db 10
 
-section .text
-    global _start
 
-_start:
 
-    mov rax, chocolat
-    add rax, 4
-    ; tenter de charger l'adresse mémoire de chocolat dans un registre
-    mov rax, [chocolat]
 
-    ; Terminer le programme
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
-
-function_retour:
-
-    ADD rsp,2
-    
